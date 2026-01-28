5f8466efefdae69ad80bf92fc845505d0d53fcf5
Remener99 <94922280+Remener99@users.noreply.github.com>
2026-01-28 16:04:04 +0300

    Create popup.tsx

---
+++ src/popup.tsx
@@ -0,0 +1,10 @@
+import TodayView from "./ui/TodayView";
+
+export default function Popup() {
+  return (
+    <div style={{ width: 300, padding: 16 }}>
+      <h3>AI-тьютор</h3>
+      <TodayView />
+    </div>
+  );
+}
